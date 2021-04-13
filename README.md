3b850ee96d3c3a3cfd56ce25ad80d818cbe4393a
Blissblass <lolpo12345@outlook.com>
2021-04-13 19:22:27 +0300

    Final Commit (Modified JS file to finalise game)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 This is a rock paper scissors project for javascript!
 It's also my first js project!
+
+What i learned from this project: 
+
+1. How to choose random values
+2. How to use math.floor
+3. How to compare variables
+4. How to use if and if else statements inside other if and if else statements
+5. And many other useful things!
--- indexjavascript.js
+++ indexjavascript.js
@@ -15,41 +15,87 @@ function computerPlay() {
  }
 }
 
+let playerScore = 0;
+let computerScore = 0;
 const computerSelection = computerPlay();
-const playerSelection = 'Paper';
+const playerSelection = prompt("Please choose Rock, Paper or Scissors");
+function game() {
+    playRound(playerSelection, computerSelection)
+    console.log(playRound(playerSelection, computerSelection));
+    playRound(playerSelection, computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    playRound(playerSelection, computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    playRound(playerSelection, computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    playRound(playerSelection, computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+   
+    if(playerScore===computerScore) {  
+        console.log("It's a tie!") 
+        computerScore = 0
+        playerScore = 0
+    } 
+    else if(playerScore>computerScore) {
+        console.log("Player Wins!")
+        computerScore = 0
+        playerScore = 0
+    }
+    else if(playerScore<computerScore) {
+        console.log("Computer Wins!")
+        computerScore = 0
+        playerScore = 0
+    }
+    }
 
-function playRound(playerSelection, computerSelection) {
+    function playRound(playerSelection, computerSelection) {
     if(computerSelection==='Rock') {
      if(playerSelection==='Rock') {
            return 'Rock and Rock, its a tie!'
         }
      if(playerSelection==='Paper') {
+         computerScore++;
          return 'Rock beats Paper, sorry bud!'
      }   
      if(playerSelection==='Scissors') {
+         playerScore++;
          return 'Scissors beats Paper, congrats!'
+     }
+     else {
+         return 'Please choose Rock Paper or Scissors!'
      }
     }
     if(computerSelection==='Paper') {
         if(playerSelection==='Rock') {
+            computerScore++;
             return 'Paper beats Rock, sorry bud!'
         }
         if(playerSelection==='Paper') {
             return 'Paper and Paper, its a tie!'
         }
         if(playerSelection==='Scissors') {
+            playerScore++;
             return 'Scissors beats Paper, congrats!'
+        }
+        else {
+            return 'Please choose Rock Paper or Scissors!'
         }
     }
      if(computerSelection==='Scissors') {
         if(playerSelection==='Rock') {
+            playerScore++;
             return 'Rock beats Scissors, congrats!'
         }
         if(playerSelection==='Paper') {
+            computerScore++;
             return 'Scissors beats Paper, sorry bud!'
         }
         if(playerSelection==='Scissors') {
             return 'Scissors and Scissors, its a tie!'
         }
+        else {
+            return 'Please choose Rock Paper or Scissors!'
+        }
      }    
     }
+
