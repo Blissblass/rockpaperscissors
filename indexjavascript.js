53bee579f2752f7db6f1b11538e4c5ba64108317
Blissblass <lolpo12345@outlook.com>
2021-04-13 16:22:53 +0300

    Add computerPlay function

--- indexjavascript.js
+++ indexjavascript.js
@@ -1,1 +1,19 @@
 console.log("Test message: If youre seeing this everythings okay");
+
+function computerPlay() {
+    const randomNumber = (Math.floor(Math.random() * 3));
+ if(randomNumber===0) {
+     return 'Rock';
+ }
+
+ else if(randomNumber===1) {
+     return 'Paper';
+ }
+
+ else if(randomNumber===2){
+     return 'Scissors';
+ }
+}
+
+function playerSelection() {
+}
