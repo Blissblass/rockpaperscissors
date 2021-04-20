20c7dfe55ea03afa6ccd63d026ca3ea9c9363c4d
Blissblass <lolpo12345@outlook.com>
2021-04-20 21:27:38 +0300

    Final commit(for real this time)

--- indexjavascript.js
+++ indexjavascript.js
@@ -1,8 +1,7 @@
 console.log("Test message: Test Message");
 
-// Random number generator that assigns Rock Paper or Scissors based on the rounded number
 function computerPlay() {
-    let randomNumber = (Math.floor(Math.random() * 3));
+    const randomNumber = (Math.floor(Math.random() * 3));
  if(randomNumber===0) {
      return 'Rock';
  }
@@ -16,7 +15,6 @@ function computerPlay() {
  }
 }
 
-// Variables and what not, could probably find a better way to take user input but meh
 let playerScore = 0;
 let computerScore = 0;
 let computerSelection = computerPlay();
@@ -54,9 +52,7 @@ function game() {
     }
     }
 
-
-// Plays a round of rock paper scissors, works flawlessly but the game() function is bugged
-function playRound(playerSelection, computerSelection) {
+    function playRound(playerSelection, computerSelection) {
     if(computerSelection==='Rock') {
      if(playerSelection==='Rock') {
            return 'Rock and Rock, its a tie!'
@@ -107,35 +103,3 @@ function playRound(playerSelection, computerSelection) {
      }    
     }
 
- //This seems to take the "computerSelection" variable only once and uses it 5 times, need to find a way to
- // make it call the function 5 times (looping probably works but i dunno how to do that yet lol)   
-function game() {
-    playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-   
-    if(playerScore===computerScore) {  
-        console.log("It's a tie!") 
-        computerScore = 0
-        playerScore = 0
-    } 
-    else if(playerScore>computerScore) {
-        console.log("Player Wins!")
-        computerScore = 0
-        playerScore = 0
-    }
-    else if(playerScore<computerScore) {
-        console.log("Computer Wins!")
-        computerScore = 0
-        playerScore = 0
-    }
-    }
-
-   
